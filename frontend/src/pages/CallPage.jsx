bddc5b99900aee1f366a1777b257db0e208e903b
Surya Sujit <35captainmarvel@gmail.com>
2025-08-08 12:42:03 +0530

    fixed api

--- frontend/src/pages/CallPage.jsx
+++ frontend/src/pages/CallPage.jsx
@@ -19,7 +19,7 @@ import "@stream-io/video-react-sdk/dist/css/styles.css";
 import toast from "react-hot-toast";
 import PageLoader from "../components/PageLoader";
 
-const STREAM_API_KEY = import.meta.env.VITE_STREAM_API_KEY;
+const STREAM_API_KEY = import.meta.env.VITE_STREAM_API_KEY || "cby9b6rn2snb";
 
 const CallPage = () => {
   const { id: callId } = useParams();
